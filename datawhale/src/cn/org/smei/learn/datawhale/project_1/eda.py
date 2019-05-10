cda118cb506af7fffdb6595278afb0c1e02f10bd
龚 崇金 <gdzsgcj@126.com>
2019-05-10 23:05:27 +0800

    搭好了架子,第一次将框架提交

---
+++ datawhale/src/cn/org/smei/learn/datawhale/project_1/eda.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2019年5月10日
+
+@author: gdgzg
+'''
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+import time
+import seaborn as sns
+plt.style.use('ggplot')
+plt.rcParams['figure.figsize'] = 10,6
+warnings.filterwarnings('ignore')
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+
